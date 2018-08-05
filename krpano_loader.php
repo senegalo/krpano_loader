d248ba045142ea2626e7d9fc3f2ddd8bbc71f236
Karim Mansour <senegalo@gmail.com>
2018-08-05 13:25:21 +0200

    initial plugin structure

---
+++ krpano_loader.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package krpano_loader
+ * @version 0.1.0 
+ */
+/*
+Plugin Name: KRpano Loader
+Plugin URI: https://github.com/senegalo/krpano_loader
+Description: This is a simple plugin that load KRpano panoramas and gives a sort of interface to be able to get .
+Author: Karim Mansour
+Version: 0.1.0
+*/
+
+
+
+?>
