ffbbc6d1057837b008e4073159bf66b5154a9ba4
Karim Mansour <senegalo@gmail.com>
2018-08-12 19:05:23 +0200

    Pano Embedding in WP
    * Created a shortcode for wordpress to embed KRPano tours
    * Shortcode is capable of modifying tour, width and height

--- krpano_loader.php
+++ krpano_loader.php
@@ -12,5 +12,32 @@ Version: 0.1.0
 */
 
 
+/**
+ * KRpano Shortcode
+ * responsds to shortcodes like: [krpano tour="tour1"]
+ * and returns the proper code to include the panorama
+ */
 
+function krpano($args) {
+  $tour_path = tour_path($args['tour']);
+  ob_start()
+?>
+<div id="pano" style="width:<?php echo $args['width']; ?>px; height:<?php echo $args['height']; ?>px;"></div>
+  <script src="<?php echo $tour_path; ?>/tour.js"></script>
+  <script>
+    embedpano({swf:"<?php echo $tour_path; ?>/tour.swf", xml:"<?php echo $tour_path; ?>/tour.xml", target:"pano"});
+  </script>
+<?php
+  return ob_get_clean();
+}
+
+/**
+ * Returns the tour root path.
+ * Assumes that the tours are stored in the uploads/tours
+ */
+function tour_path($tour_name){
+  return wp_upload_dir(null, false)["baseurl"]."/tours/".$tour_name;
+}
+
+add_shortcode('krpano', 'krpano');
 ?>
